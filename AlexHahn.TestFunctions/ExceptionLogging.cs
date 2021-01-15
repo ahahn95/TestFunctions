fc44acf75b2cd572fac0bff9b343090a78966d62
Alexander Hahn <ahahn95@gmail.com>
2021-01-15 12:22:30 -0500

    init

---
+++ AlexHahn.TestFunctions/ExceptionLogging.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AlexHahn.TestFunctions
+{
+    public class ExceptionLogging
+    {
+        private readonly TelemetryClient _telemetryClient;
+
+        public ExceptionLogging(TelemetryConfiguration telemetryConfiguration)
+        {
+            _telemetryClient = new TelemetryClient(telemetryConfiguration);
+        }
+
+        [FunctionName("ExceptionLogging")]
+        public void Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
+            HttpRequest req, ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            try
+            {
+                if (true)
+                {
+                    throw new ArgumentException();
+                }
+            }
+            catch (Exception e)
+            {
+                _telemetryClient.TrackException(e);
+            }
+        }
+    }
+}
